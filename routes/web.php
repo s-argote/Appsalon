204e20eb24f9d43951ef7a9848414dc297ed37ca
Santiago Argote <argotesantiago38@gmail.com>
2025-09-20 17:09:18 -0500

    Conexion con BD

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/probar-db', function () {
+    try {
+        DB::connection()->getPdo();
+        return "¡Conexión a la base de datos exitosa!";
+    } catch (\Exception $e) {
+        return "Error al conectar con la base de datos: " . $e->getMessage();
+    }
 });
